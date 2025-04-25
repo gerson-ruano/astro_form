2a01a73a17525a63c273d45bb8db161e89154dd7
gruano <toge619@gmail.com>
2025-04-24 20:39:20 -0600

    inicio de astro forms para registrar asistencia

---
+++ src/data/options.ts
@@ -0,0 +1,29 @@
+export const grados = [
+    "Agente", "Subispector", "Inspector", "Oficial Tercero", "Oficial Segundo", 
+    "Oficial Primero", "Subcomisario", "Comisario", "Comisario General", "Subdirector",  "Director", "Ninguno"
+  ];
+  
+  export const roles = [
+    "Operador de Planta de Transmisiones", "Analista", "Oficinista de Atención Ciudadana", "Oficinista de Delegacion Especilidad"
+    , "Jefe de servicios", "Agente Operativo", "Llamadas 110", "Coordinación 110", "Otros"
+  ];
+  
+  export const guardias = [
+    "Guardia A", "Guardia B", "Grupo 3"
+  ];
+  
+  export const destinos = [
+    "Comisaría 11", "Comisaría 12", "Comisaría 13", "Comisaría 14", "Comisaría 15", "Comisaría 16", "Comisaría 21"
+    , "Comisaría 22", "Comisaría 23", "Comisaría 24", "Comisaría 31", "Comisaría 32", "Comisaría 33", "Comisaría 34"
+    , "Comisaría 41", "Comisaría 42", "Comisaría 43", "Comisaría 44", "Comisaría 51", "Comisaría 52", "Comisaría 53"
+    , "Comisaría 61", "Comisaría 62", "Comisaría 71", "Comisaría 72", "Comisaría 73", "Comisaría 74", "Distrito 1"
+    , "Distrito 2", "Distrito 3", "Distrito 4", "Distrito 5", "Distrito 6", "Distrito 7", "DISETUR", "POLIMERC",
+     "DIPRONA", "DIPANDA", "CENTRO DE LLAMADAS 110"
+  ];
+  
+  export const subdestinos = {
+    "Comisaría 11": ["Huehuetenango", "Quiché"],
+    "Comisaría 12": ["Escuintla", "Santa Rosa"],
+    "Comisaría 13": ["Ciudad Capital", "Mixco"]
+  };
+  
