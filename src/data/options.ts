8383a41176b5ee8349b7f34d202de20ab77b19de
gersonruano <toge619@gmail.com>
2025-04-26 23:36:30 -0600

    add validaciones, notificaciones, mejora de subdestinos, mejora de redireccion a success

--- src/data/options.ts
+++ src/data/options.ts
@@ -1,11 +1,13 @@
 export const grados = [
-    "Agente", "Subispector", "Inspector", "Oficial Tercero", "Oficial Segundo", 
-    "Oficial Primero", "Subcomisario", "Comisario", "Comisario General", "Subdirector",  "Director", "Ninguno"
+    "Asesor", "Adminitrativo Subgrupo 18", "Administrativo 0-29","Administrativo 0-11","Agente"
+    , "Subispector", "Inspector", "Oficial Tercero", "Oficial Segundo", "Oficial Primero", "Subcomisario"
+    , "Comisario", "Comisario General", "Subdirector",  "Director", "Ninguno"
   ];
   
   export const roles = [
-    "Operador de Planta de Transmisiones", "Analista", "Oficinista de Atención Ciudadana", "Oficinista de Delegacion Especilidad"
-    , "Jefe de servicios", "Agente Operativo", "Llamadas 110", "Coordinación 110", "Otros"
+    "Operador de Planta de Transmisiones", "Analista y Estadística", "Planificación", "Jefe de Sección de Operaciones"
+    , "Jefe de Sección de Personal", "Oficinista de Atención Ciudadana", "Oficinista de Delegacion Especilidad", "Jefe de servicios"
+    , "Agente Operativo", "Llamadas 110", "Coordinación 110", "Otros"
   ];
   
   export const guardias = [
@@ -16,14 +18,18 @@ export const grados = [
     "Comisaría 11", "Comisaría 12", "Comisaría 13", "Comisaría 14", "Comisaría 15", "Comisaría 16", "Comisaría 21"
     , "Comisaría 22", "Comisaría 23", "Comisaría 24", "Comisaría 31", "Comisaría 32", "Comisaría 33", "Comisaría 34"
     , "Comisaría 41", "Comisaría 42", "Comisaría 43", "Comisaría 44", "Comisaría 51", "Comisaría 52", "Comisaría 53"
-    , "Comisaría 61", "Comisaría 62", "Comisaría 71", "Comisaría 72", "Comisaría 73", "Comisaría 74", "Distrito 1"
-    , "Distrito 2", "Distrito 3", "Distrito 4", "Distrito 5", "Distrito 6", "Distrito 7", "DISETUR", "POLIMERC",
-     "DIPRONA", "DIPANDA", "CENTRO DE LLAMADAS 110"
+    , "Comisaría 61", "Comisaría 62", "Comisaría 71", "Comisaría 72", "Comisaría 73", "Comisaría 74", "Distrito 1 Central"
+    , "Distrito 2 Oriente", "Distrito 3 Sur", "Distrito 4 Occidente", "Distrito 5 Nororiente", "Distrito 6 Norte"
+    , "Distrito 7 Noroccidente", "DISETUR", "POLIMERC", "DIPRONA", "DIPANDA", "CENTRO DE LLAMADAS 110", "DOC"
   ];
   
   export const subdestinos = {
-    "Comisaría 11": ["Huehuetenango", "Quiché"],
-    "Comisaría 12": ["Escuintla", "Santa Rosa"],
-    "Comisaría 13": ["Ciudad Capital", "Mixco"]
+    "Comisaría 11": ["Estación 11-1 LA MERCED", "Subestación 11-1-1 BARRANQUILLA", "Subestación 11-1-2 BARRIO GERONA", "Subestación 11-1-3 EL TUERTO", "Subestación 11-1-4 DIEZ DE MAYO", "Subestación 11-1-5 BARRIO MODERNO", "Subestación 11-1-6 MORAZAN", "Subestación 11-1-7 EL ZAPOTE", "Estación 11-2 ZONA 3", "Estación 11-3 LA TERMINAL", "Estación 11-4 ZONA 8", "Estación 11-5 CENTRO CÍVICO", "Subestación 11-5-1", "Subestación 11-5-2 CH NORTE", "Subestación 11-5-3 CH SUR"],
+    "Comisaría 12": ["Estación 12-1 PALENCIA", "Subestación 12-1-1 PALENCIA", "Subestación 12-1-2 LOS MIXCOS", "Subestación 12-1-2 LOS MIXCOS", "Subestación 12-1-3 EL FISCAL", "Subestación 12-1-4 EL CANTON", "Subestación 12-1-5 SAN JOSÉ DEL GOLFO", "Subestación 12-1-6 SAN SUR", "Estación 12-2 ALAMEDAS", "Subestación 12-2-1 ALAMEDAS", "Subestación 12-2-2 SAN RAFAEL", "Subestación 12-2-3 PINARES", "Subestación 12-2-4 PARAISOS", "Subestación 12-2-5 VALLE", "Subestación 12-2-6 LO DE RODRIGUEZ", "Subestación 12-2-7 SANTA MONICA", "Subestación 12-2-9 EL CHATO", "Subestación 12-2-10 LLANO LARGO", "Estación 12-3 ILUSIONES", "Subestación 12-3-1 ILUSIONES", "Subestación 12-3-2 SANTA FAZ", "Subestación 12-3-3 KENNEDY", "Subestación 12-3-4 LA MAYA", "Subestación 12-3-5 ENCINOS", "Subestación 12-3-6 EL LIMON", "Subestación 12-3-7 HOLANDA", "Subestación 12-3-8 CANAAN", "Subestación 12-3-9 SANTA ELENA III", "Estación 12-4 LOMAS DEL NORTE", "Subestación 12-4-1 LOMAS DEL NORTE", "Subestación 12-4-2 CANALITOS", "Subestación 12-4-3 SABANA ARRIBA", "Subestación 12-4-4 LAS CONCHAS", "Estación 12-5 CIPRESALES", "Subestación 12-5-1 CIPRESALES", "Subestación 12-5-2 SAN MARTIN", "Subestación 12-5-3 LA PARROQUIA", "Subestación 12-5-4 NUEVO BARRIO", "Subestación 12-5-5 BARRIO SAN ANTONIO", "Subestación 12-5-6 JESUS BUENA ESPE", "Subestación 12-5-7 SAN JUAN DE DIOS", "Subestación 12-5-8 PROYECTOS 4-10", "Subestación 12-5-9 LA REYNITA","Estación 12-6 SANTA LUISA", "Subestación 12-6-1 SANTA LUISA", "Subestación 12-6-2 SANTA ISABEL", "Subestación 12-6-3 SAN JULIAN", "Subestación 12-6-4 SANTA FAZ", "Subestación 12-6-5 SAN ANTONIO LFS", "Estación 12-7 SAN PEDRO AYAMPUC", "Subestación 12-7-1 SAN PEDRO AYAMPUC", "Subestación 12-7-2 ALTOS DE SANTA MARÍA", "Subestación 12-7-3 RIO AZUL BRISAS"],
+    "Comisaría 13": ["Escuintla", "Santa Rosa"],
+    "Comisaría 14": ["Ciudad Capital", "Mixco"]
   };
+  
+ 
+
   
