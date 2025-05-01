60460db0cdbd951a58e43b47108e8073f75c0be8
gruano <toge619@gmail.com>
2025-05-01 13:57:29 -0600

    fix envio de formulario, organizacion de subdestinos

--- src/data/options.js
+++ src/data/options.js
@@ -27,48 +27,7 @@ export const grados = [
     , "Distrito 7 Noroccidente", "DISETUR", "POLIMERC", "DIPRONA", "DIPANDA", "CENTRO DE LLAMADAS 110", "DOC"
   ];
   
-  export const subdestinos = {
-    "Comisaría 11": ["Estación 11-1 LA MERCED", "Subestación 11-1-1 BARRANQUILLA", "Subestación 11-1-2 BARRIO GERONA", "Subestación 11-1-3 EL TUERTO", "Subestación 11-1-4 DIEZ DE MAYO", "Subestación 11-1-5 BARRIO MODERNO", "Subestación 11-1-6 MORAZAN", "Subestación 11-1-7 EL ZAPOTE", "Estación 11-2 ZONA 3", "Estación 11-3 LA TERMINAL", "Estación 11-4 ZONA 8", "Estación 11-5 CENTRO CÍVICO", "Subestación 11-5-1", "Subestación 11-5-2 CH NORTE", "Subestación 11-5-3 CH SUR"],
-    "Comisaría 12": ["Estación 12-1 PALENCIA", "Subestación 12-1-1 PALENCIA", "Subestación 12-1-2 LOS MIXCOS", "Subestación 12-1-2 LOS MIXCOS", "Subestación 12-1-3 EL FISCAL", "Subestación 12-1-4 EL CANTON", "Subestación 12-1-5 SAN JOSÉ DEL GOLFO", "Subestación 12-1-6 SAN SUR", "Estación 12-2 ALAMEDAS", "Subestación 12-2-1 ALAMEDAS", "Subestación 12-2-2 SAN RAFAEL", "Subestación 12-2-3 PINARES", "Subestación 12-2-4 PARAISOS", "Subestación 12-2-5 VALLE", "Subestación 12-2-6 LO DE RODRIGUEZ", "Subestación 12-2-7 SANTA MONICA", "Subestación 12-2-9 EL CHATO", "Subestación 12-2-10 LLANO LARGO", "Estación 12-3 ILUSIONES", "Subestación 12-3-1 ILUSIONES", "Subestación 12-3-2 SANTA FAZ", "Subestación 12-3-3 KENNEDY", "Subestación 12-3-4 LA MAYA", "Subestación 12-3-5 ENCINOS", "Subestación 12-3-6 EL LIMON", "Subestación 12-3-7 HOLANDA", "Subestación 12-3-8 CANAAN", "Subestación 12-3-9 SANTA ELENA III", "Estación 12-4 LOMAS DEL NORTE", "Subestación 12-4-1 LOMAS DEL NORTE", "Subestación 12-4-2 CANALITOS", "Subestación 12-4-3 SABANA ARRIBA", "Subestación 12-4-4 LAS CONCHAS", "Estación 12-5 CIPRESALES", "Subestación 12-5-1 CIPRESALES", "Subestación 12-5-2 SAN MARTIN", "Subestación 12-5-3 LA PARROQUIA", "Subestación 12-5-4 NUEVO BARRIO", "Subestación 12-5-5 BARRIO SAN ANTONIO", "Subestación 12-5-6 JESUS BUENA ESPE", "Subestación 12-5-7 SAN JUAN DE DIOS", "Subestación 12-5-8 PROYECTOS 4-10", "Subestación 12-5-9 LA REYNITA","Estación 12-6 SANTA LUISA", "Subestación 12-6-1 SANTA LUISA", "Subestación 12-6-2 SANTA ISABEL", "Subestación 12-6-3 SAN JULIAN", "Subestación 12-6-4 SANTA FAZ", "Subestación 12-6-5 SAN ANTONIO LFS", "Estación 12-7 SAN PEDRO AYAMPUC", "Subestación 12-7-1 SAN PEDRO AYAMPUC", "Subestación 12-7-2 ALTOS DE SANTA MARÍA", "Subestación 12-7-3 RIO AZUL BRISAS"],
-    "Comisaría 13": ["Escuintla", "Santa Rosa"],
-    "Comisaría 14": ["Ciudad Capital", "Mixco"],
-    "Comisaría 15": ["Ciudad Capital", "Mixco"],
-    "Comisaría 16": ["Ciudad Capital", "Mixco"],
-    "Comisaría 21": ["Ciudad Capital", "Mixco"],
-    "Comisaría 22": ["Ciudad Capital", "Mixco"],
-    "Comisaría 23": ["Ciudad Capital", "Mixco"],
-    "Comisaría 24": ["Ciudad Capital", "Mixco"],
-    "Comisaría 31": ["Ciudad Capital", "Mixco"],
-    "Comisaría 32": ["Ciudad Capital", "Mixco"],
-    "Comisaría 33": ["Ciudad Capital", "Mixco"],
-    "Comisaría 34": ["Ciudad Capital", "Mixco"],
-    "Comisaría 41": ["Ciudad Capital", "Mixco"],
-    "Comisaría 42": ["Ciudad Capital", "Mixco"],
-    "Comisaría 43": ["Ciudad Capital", "Mixco"],
-    "Comisaría 44": ["Ciudad Capital", "Mixco"],
-    "Comisaría 51": ["Ciudad Capital", "Mixco"],
-    "Comisaría 52": ["Ciudad Capital", "Mixco"],
-    "Comisaría 53": ["Ciudad Capital", "Mixco"],
-    "Comisaría 61": ["Ciudad Capital", "Mixco"],
-    "Comisaría 62": ["Ciudad Capital", "Mixco"],
-    "Comisaría 71": ["Ciudad Capital", "Mixco"],
-    "Comisaría 72": ["Ciudad Capital", "Mixco"],
-    "Comisaría 73": ["Ciudad Capital", "Mixco"],
-    "Comisaría 74": ["Ciudad Capital", "Mixco"],
-    "Distrito 1 Central": ["Ciudad Capital", "Mixco"],
-    "Distrito 2 Oriente": ["Ciudad Capital", "Mixco"],
-    "Distrito 3 Sur": ["Ciudad Capital", "Mixco"],
-    "Distrito 4 Occidente": ["Ciudad Capital", "Mixco"],
-    "Distrito 5 Nororiente": ["Ciudad Capital", "Mixco"],
-    "Distrito 6 Norte": ["Ciudad Capital", "Mixco"],
-    "Distrito 7 Noroccidente": ["Ciudad Capital", "Mixco"],
-    "DISETUR": ["Ciudad Capital", "Mixco"],
-    "POLIMERC": ["Ciudad Capital", "Mixco"],
-    "DIPRONA": ["Ciudad Capital", "Mixco"],
-    "DIPANDA": ["Ciudad Capital", "Mixco"],
-    "CENTRO DE LLAMADAS 110": ["Ciudad Capital", "Mixco"],
-    "DOC": ["Ciudad Capital", "Mixco"],
-  };
+  
   
  
 
